e7154525ed042cf2d9c0567b59f12a2d11f4139e
Priscila Cunha <priscilacunha@msn.com>
2023-05-20 12:58:38 -0300

    Initial Commit

---
+++ App.js
@@ -0,0 +1,42 @@
+import { StatusBar } from 'expo-status-bar';
+import { FlatList, ScrollView, StyleSheet, Text, View } from 'react-native';
+
+const buildData = () => {
+  const data = []
+  for (let i = 0; i < 100; i++) {
+    data.push(i)
+  }
+  return data
+}
+
+// const BasicItem = (props) => <View><Text>Item n: {props.numero}</Text></View>
+const BasicItem = ({numero, nome}) => <View><Text>Item n: {numero} - {nome}</Text></View>
+
+export default function App() {
+
+  return (
+    <ScrollView style={styles.scrollView}>
+      <FlatList
+        data={buildData()}
+        renderItem={({item}) => (
+          <BasicItem key={item} nome="teste" numero={item} />
+        )}
+        keyExtractor={item => item}
+      />
+
+      <StatusBar style="auto" />
+    </ScrollView>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  scrollView: {
+    backgroundColor: 'pink',
+  }
+});
